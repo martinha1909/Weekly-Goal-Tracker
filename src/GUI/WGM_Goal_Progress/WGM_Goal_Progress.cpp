cd9ae6260892aaaede7c87d0e51518c16336c19a
Martin Ha <vu.ha1@ucalgary.ca>
2023-08-19 23:25:36 -0600

    WGM_CheckList

--- src/GUI/WGM_Goal_Progress/WGM_Goal_Progress.cpp
+++ src/GUI/WGM_Goal_Progress/WGM_Goal_Progress.cpp
@@ -1,3 +1,5 @@
+#include <wx/checkbox.h>
+#include <wx/checklst.h>
 #include "include/WGM_Goal_Progress.h"
 #include "../Components/WGM_StaticText/include/WGM_StaticText.h"
 
@@ -14,8 +16,34 @@ WGM_Goal_Progress::~WGM_Goal_Progress()
 
 void WGM_Goal_Progress::show()
 {
-	wxStaticText* titleLabel = new WGM_StaticText(frame, wxID_ANY, "Title Text", wxPoint(380, 98), wxDefaultSize, wxALIGN_CENTER);
+	if (frame) {
+		int row = 120;
+		wxStaticText* titleLabel = new WGM_StaticText(frame, wxID_ANY, "Title Text", wxPoint(380, 80), wxDefaultSize, wxALIGN_CENTER);
+		
+		progress = new wxGauge(frame, wxID_ANY, 100, wxPoint(450, 80), wxSize(300, 20), wxGA_HORIZONTAL);
+		progress->SetValue(20); // Set progress to 20%
 
-	progress = new wxGauge(frame, wxID_ANY, 100, wxPoint(450, 100), wxSize(300, 15), wxGA_HORIZONTAL);
-	progress->SetValue(20); // Set progress to 20%
+		for (int i = 0; i < 3; i++) {
+			sub_goals.push_back(new WGM_CheckList(frame, wxID_ANY, wxPoint(450, row), wxSize(300, 25)));
+			sub_goals[sub_goals.size() - 1]->Append(std::to_string(sub_goals[sub_goals.size() - 1]->GetId()));
+			sub_goals[sub_goals.size() - 1]->Bind(wxEVT_CHECKLISTBOX, &WGM_Goal_Progress::onSubGoalComplete, this);
+			row += 35;
+		}
+	}
+}
+
+void WGM_Goal_Progress::onSubGoalComplete(wxCommandEvent& event)
+{
+	int id = event.GetId();
+	int item_id = event.GetInt();
+
+	for (auto sub_goal : sub_goals) {
+		if (sub_goal->GetId() == id) {
+			sub_goal = new WGM_CheckList(frame, wxID_ANY, wxPoint(450, 120), wxSize(300, 25), wxColour(0, 255, 0));
+			/*sub_goal->SetBackgroundColour(wxColour(0, 255, 0));
+			sub_goal->Layout();*/
+			break;
+		}
+	}
+	event.Skip();
 }
--- src/GUI/WGM_Goal_Progress/include/WGM_Goal_Progress.h
+++ src/GUI/WGM_Goal_Progress/include/WGM_Goal_Progress.h
@@ -3,6 +3,7 @@
 #include <wx/wx.h>
 #include <wx/gauge.h>
 #include "../../../Driver/Goal/include/Goal.h"
+#include "../../Components/WGM_CheckList/include/WGM_CheckList.h"
 
 class Goal;
 
@@ -12,9 +13,11 @@ private:
 	Goal* goal = nullptr;
 	wxFrame* frame = nullptr;
 	wxGauge* progress = nullptr;
+	std::vector<WGM_CheckList*> sub_goals;
 public:
 	WGM_Goal_Progress(Goal* goal, wxFrame* frame);
 	~WGM_Goal_Progress();
 	void show();
+	void onSubGoalComplete(wxCommandEvent& event);
 };
 
